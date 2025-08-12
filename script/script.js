b0fd7c0bb3f1110fa41815bb00ddd81a7c016506
Henrique Uchida <90984644+HenriqueUchida@users.noreply.github.com>
2025-08-12 20:25:13 +0000

    adicionando CSS e iniciando JS

--- script/script.js
+++ script/script.js
@@ -1,1 +1,20 @@
-//alert(`Salve salve`)
+let rodadaAnterior = document.querySelector('#btn-anterior');
+let rodadaProxima = document.querySelector('#btn-proximo');
+let identicadorRodada = document.querySelector('.rodada')
+conteudoRodada = identifcador.value
+rodadaAnterior.addEventListener('click', voltar());
+rodadaProxima.addEventListener('click', avancar());
+let rodadaInicial = 1;
+const totalRodadas = 3;
+
+
+
+function voltar() {
+    botaoVoltar.disabled = true;
+    if (conteudoRodada != '1ª RODADA') {
+        botaoVoltar.disabled = false;
+        identificadorRodada.innerHTML = ''
+
+    };
+        
+};
